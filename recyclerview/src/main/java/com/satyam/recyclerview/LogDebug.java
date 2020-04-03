1c2034162716ea0448b61129e3caccc10e319476
Arvind Kumar <ak010cse@gmail.com>
2020-04-03 11:29:33 +0530

    Check

---
+++ recyclerview/src/main/java/com/satyam/recyclerview/LogDebug.java
@@ -0,0 +1,11 @@
+package com.satyam.recyclerview;
+
+import android.util.Log;
+
+public class LogDebug {
+    public static final String TAG = "ARVIND_TEST";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+}
